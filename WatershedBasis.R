38e3e802102e852a414b9ed0ce1519d5e7608b33
Christian Carson <60585941+christiancarson@users.noreply.github.com>
2022-12-06 19:17:56 -0700

    Major loops!

--- WatershedBasis.R
+++ WatershedBasis.R
@@ -36,7 +36,6 @@ library(plyr)
 library(dplyr)
 library(ggplot2)
 library(tibble)
-library(car)
 library(reshape2)
 library(epitools)
 library(readxl)
@@ -56,7 +55,6 @@ library(reshape2)
 library(dplyr)
 library(ggplot2)
 library(knitr)
-#install.packages("vtable")
 library(vtable)
 #install.packages("ggrepel")
 library(ggrepel)
@@ -112,34 +110,31 @@ figures.path <- paste(wd, "/", folders[2], sep = "")
 #on in the program
 data.path <- paste(wd, "/", "Data", sep = "")
 
+#####you will need to find the exact path of the FINAL_FWRA_RESULTS_ALL_AREAS_MASTER path on our sharepoint and paste it below
+                       
+FWRA <- read_excel("/Users/critty/Desktop/Dekstop/GitHub/FWRA_2022_Analysis/Data/FINAL_FWRA_RESULTS_ALL_AREAS_MASTER.xlsx", sheet = 1)
+head(FWRA)
 
-FWRA <- read.csv(paste(data.path,"/", "Area23_Watersheds_Cleaned.csv",
-                       sep = ""), stringsAsFactors = FALSE)
-
-FWRA <- subset(FWRA, LF != "LF23" & LF != "LF24")
+FWRA <- subset(FWRA, LF != "23" & LF != "24")
 
 # time to upload the datas
 watersheds <- c(print(unique(FWRA$W)))
-
-st(FWRA)
-Gaps <- subset(FWRA, CR <= 0)
-Risks<-subset(FWRA, CR= 0 & CR!= -1)
-unique(Gaps$TR)
-count((subset(FWRA, TR = 0)))
-count((subset(FWRA, TR = 1)))
-
-
-
-options(ggrepel.max.overlaps = Inf)
+  
+ options(ggrepel.max.overlaps = Inf)
 for (i in watersheds) {
   
 Sarita <- subset(FWRA, FWRA$W == i)
 
 Sarita<-subset(Sarita, CR!= 0 & CR!= -1)
+FWRA$CR <- as.numeric(FWRA$CR)
+FWRA$FR <- as.numeric(FWRA$FR)
+FWRA$TR <- as.numeric(FWRA$TR)
+
 library(vtable)
 library(RColorBrewer)
 library(dplyr)
 library(ggplot2)
+library(ggrepel)
 
 myData <- matrix(c(2,2,3,3,3,1,2,2,3,3,1,1,2,2,3,1,1,2,2,2,1,1,1,1,2), nrow = 5, ncol = 5, byrow = TRUE)
 longData <- reshape2::melt(myData)
@@ -163,7 +158,7 @@ labels=c("VL","L","M","H","VH"),
  coord_fixed()+ 
   geom_point(data = Sarita, aes(), size = 3, color = "white") +
   geom_label_repel(data = Sarita,
-    aes(label = LF),
+    aes(label = LF, fill = TR),
     fontface = 'bold', color = 'white',size = 5,
     box.padding = unit(0.35, "lines"),
     point.padding = unit(0.5, "lines"),
@@ -178,7 +173,7 @@ labels=c("VL","L","M","H","VH"),
 
   
 #Save the plot, define your folder location as "/Users/user/Documents/GitHub/FWRA_2022_Analysis/Figures"
-ggsave(filename = paste0("/Users/critty/Desktop/Dekstop/GitHub/FWRA_2022_Analysis/Figures/Area_23_Figures/", print(i),".jpeg"),
+ggsave(filename = paste0("/Users/critty/Desktop/Dekstop/GitHub/FWRA_2022_Analysis/Figures/Watershed_Risks/", print(i),".jpeg"),
        device = "jpeg",
        width = 30,
        height = 30,
@@ -186,4 +181,118 @@ ggsave(filename = paste0("/Users/critty/Desktop/Dekstop/GitHub/FWRA_2022_Analysi
        dpi = 300)
 
 
+}
+
+###Reload for tables######
+# time to upload the datas
+####Risk Rankings
+
+FWRA <- read_excel("/Users/critty/Desktop/Dekstop/GitHub/FWRA_2022_Analysis/Data/FINAL_FWRA_RESULTS_ALL_AREAS_MASTER.xlsx", sheet = 1)
+head(FWRA)
+
+FWRA <- subset(FWRA, LF != "23" & LF != "24")
+FWRA<-subset(FWRA, CR!= 0 & CR!= -1 & FR!= 0 & FR!= -1)
+
+FWRA <- select(FWRA,W,LF_Name,TR,CR,FR)
+
+FWRA <- subset(FWRA, LF_Name != "LF22: Mortality or fitness reduction resulting from frequent and higher peak flows causing redd scour" & LF_Name != "LF23: Mortality of eggs during incubation due to variable lake water levels")
+unique(FWRA$LF_Name)
+
+watersheds <- c(print(unique(FWRA$W)))
+
+for (i in watersheds) {
+
+SARITA_NUMERIC <- dplyr::filter(FWRA, W %in% c(print(i)))
+colnames(SARITA_NUMERIC)
+SARITA_NUMERIC$TR <- as.numeric(SARITA_NUMERIC$TR) 
+SARITA_NUMERIC$Rank <- rank(-SARITA_NUMERIC$TR, ties.method = "min")
+
+colnames(SARITA_NUMERIC)
+col_order <- c("W","LF_Name","Rank","TR","CR", "FR")
+
+SARITA_NUMERIC <- SARITA_NUMERIC[, col_order]
+SARITA_NUMERIC <- SARITA_NUMERIC[order(SARITA_NUMERIC$TR, decreasing = TRUE),]  
+SARITA_NUMERIC <- SARITA_NUMERIC %>% 
+  mutate(across(CR:FR, as.character))
+
+SARITA_NUMERIC$CR[SARITA_NUMERIC$CR=="1"]<-"VL"
+SARITA_NUMERIC$CR[SARITA_NUMERIC$CR=="2"]<-"L"
+SARITA_NUMERIC$CR[SARITA_NUMERIC$CR=="3"]<-"M"
+SARITA_NUMERIC$CR[SARITA_NUMERIC$CR=="4"]<-"H"
+SARITA_NUMERIC$CR[SARITA_NUMERIC$CR=="5"]<-"VH"
+SARITA_NUMERIC$FR[SARITA_NUMERIC$FR=="1"]<-"VL"
+SARITA_NUMERIC$FR[SARITA_NUMERIC$FR=="2"]<-"L"
+SARITA_NUMERIC$FR[SARITA_NUMERIC$FR=="3"]<-"M"
+SARITA_NUMERIC$FR[SARITA_NUMERIC$FR=="4"]<-"H"
+SARITA_NUMERIC$FR[SARITA_NUMERIC$FR=="5"]<-"VH"
+
+#mycols <- rev(c("red3","darkorange1","gold1","yellowgreen","forestgreen"))
+#cols <- colorRampPalette(mycols)
+
+colnames(SARITA_NUMERIC)[which(names(SARITA_NUMERIC) == "W")] <- "Watershed"
+colnames(SARITA_NUMERIC)[which(names(SARITA_NUMERIC) == "LF_Name")] <- "LF"
+colnames(SARITA_NUMERIC)[which(names(SARITA_NUMERIC) == "TR")] <- "Total Risk"
+colnames(SARITA_NUMERIC)[which(names(SARITA_NUMERIC) == "CR")] <- "Current Risk"
+colnames(SARITA_NUMERIC)[which(names(SARITA_NUMERIC) == "FR")] <- "Future Risk"
+unique(SARITA_NUMERIC$LF)
+
+SARITA_NUMERIC %>%
+    head(68) %>%
+    gt() %>%
+gtsave(filename = paste0("/Users/critty/Desktop/Dekstop/GitHub/FWRA_2022_Analysis/Figures/Watershed_Risk_Tables/", print(i),".docx"))
+
+}
+
+###Reload for tables######
+# time to upload the datas
+####Data Gaps
+
+FWRA <- read_excel("/Users/critty/Desktop/Dekstop/GitHub/FWRA_2022_Analysis/Data/FINAL_FWRA_RESULTS_ALL_AREAS_MASTER.xlsx", sheet = 1)
+head(FWRA)
+
+FWRA <- subset(FWRA, LF != "23" & LF != "24")
+  FWRA<-subset(FWRA, CR!= 1 & CR!= 2 & CR!= 3 & CR!= 4 & CR!= 5 & FR!= 1 & FR!= 2 & FR!= 3 & FR!= 4 & FR!= 5)
+
+FWRA <- select(FWRA,W,LF_Name,TR,CR,FR)
+
+FWRA <- subset(FWRA, LF_Name != "LF22: Mortality or fitness reduction resulting from frequent and higher peak flows causing redd scour" & LF_Name != "LF23: Mortality of eggs during incubation due to variable lake water levels")
+unique(FWRA$LF_Name)
+
+watersheds <- c(print(unique(FWRA$W)))
+
+for (i in watersheds) {
+
+SARITA_NUMERIC <- dplyr::filter(FWRA, W %in% c(print(i)))
+colnames(SARITA_NUMERIC)
+SARITA_NUMERIC$TR <- as.numeric(SARITA_NUMERIC$TR) 
+SARITA_NUMERIC$Rank <- rank(-SARITA_NUMERIC$TR, ties.method = "min")
+
+colnames(SARITA_NUMERIC)
+col_order <- c("W","LF_Name","Rank","TR","CR", "FR")
+
+SARITA_NUMERIC <- SARITA_NUMERIC[, col_order]
+SARITA_NUMERIC <- SARITA_NUMERIC[order(SARITA_NUMERIC$TR, decreasing = TRUE),]  
+SARITA_NUMERIC <- SARITA_NUMERIC %>% 
+  mutate(across(CR:FR, as.character))
+
+  SARITA_NUMERIC$CR[SARITA_NUMERIC$CR=="-1"]<-"HPDG"
+  SARITA_NUMERIC$CR[SARITA_NUMERIC$CR=="0"]<-"LPDG"
+  SARITA_NUMERIC$FR[SARITA_NUMERIC$FR=="-1"]<-"HPDG"
+  SARITA_NUMERIC$FR[SARITA_NUMERIC$FR=="0"]<-"LPDG"
+
+#mycols <- rev(c("red3","darkorange1","gold1","yellowgreen","forestgreen"))
+#cols <- colorRampPalette(mycols)
+
+colnames(SARITA_NUMERIC)[which(names(SARITA_NUMERIC) == "W")] <- "Watershed"
+colnames(SARITA_NUMERIC)[which(names(SARITA_NUMERIC) == "LF_Name")] <- "LF"
+colnames(SARITA_NUMERIC)[which(names(SARITA_NUMERIC) == "TR")] <- "Total Risk"
+colnames(SARITA_NUMERIC)[which(names(SARITA_NUMERIC) == "CR")] <- "Current Risk"
+colnames(SARITA_NUMERIC)[which(names(SARITA_NUMERIC) == "FR")] <- "Future Risk"
+unique(SARITA_NUMERIC$LF)
+
+  SARITA_NUMERIC %>%
+    head(68) %>%
+    gt() %>%
+    gtsave(filename = paste0("/Users/critty/Desktop/Dekstop/GitHub/FWRA_2022_Analysis/Figures/Watershed_DG_Tables/", print(i),".docx"))
+  
 }
